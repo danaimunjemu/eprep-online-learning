b2b5cc1610f84a62ddb7d7ce696bca12b97765a1
Danai Munjemu <danaimunjemu@Danais-MacBook-Pro.local>
2023-05-10 16:21:07 +0200

    Courses Page start

--- src/app/features/application/application-routing.module.ts
+++ src/app/features/application/application-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AppComponent} from "./shared/app/app.component";
+import {StudentDashboardComponent} from "./learner/pages/student-dashboard/student-dashboard.component";
+import {CoursesComponent} from "./learner/pages/courses/courses.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'app', pathMatch: 'full' },
-  { path: 'app', component: AppComponent }
+  { path: 'app', component: AppComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: StudentDashboardComponent},
+      { path: 'courses', component: CoursesComponent}
+    ]
+  },
 ];
 
 @NgModule({
--- src/app/features/application/application.module.ts
+++ src/app/features/application/application.module.ts
@@ -6,11 +6,15 @@ import { ApplicationRoutingModule } from './application-routing.module';
 import { AntDesignModules } from "../../core/modules/antdesign.module";
 import { AppComponent } from './shared/app/app.component';
 import { FormsModule } from "@angular/forms";
+import { CoursesComponent } from './learner/pages/courses/courses.component';
+import { StudentDashboardComponent } from './learner/pages/student-dashboard/student-dashboard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    StudentDashboardComponent
   ],
   imports: [
     CommonModule,
